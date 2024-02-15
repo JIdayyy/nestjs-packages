e1a8bd10c4c3cd175554feeb46330985dd0d9328
Julien Abbadie <julien.abbadie@pytheascapital.com>
2024-02-15 07:12:22 +0100

    feat: fix auth module env url and clean a bit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { MinioModule } from './libs';
 
 @Module({
-  imports: [],
+  imports: [MinioModule],
 })
 export class AppModule {}
--- src/libs/auth/jwt-strategy.service.ts
+++ src/libs/auth/jwt-strategy.service.ts
@@ -1,18 +1,19 @@
 import { ExtractJwt, Strategy, VerifiedCallback } from 'passport-jwt';
-
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { passportJwtSecret } from 'jwks-rsa';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  // @ts-expect-error - We don't need to use the constructor parameters
+  constructor(private readonly configService: ConfigService) {
     super({
       secretOrKeyProvider: passportJwtSecret({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: `http://keycloak:8080/auth/realms/wildify/protocol/openid-connect/certs`,
+        jwksUri: `${configService.get('KEYCLOAK_URL')}/realms/${configService.get('KEYCLOAK_REALM')}/protocol/openid-connect/certs`,
       }),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
